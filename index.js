f6bf3b8808ddcee7806cd977c9b3df2239a69591
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-10-08 15:13:58 -0700

    Add GET for express app.(app.get). #1

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
+const { MongoClient } = require('mongodb')
+const url = 'mongodb://localhost/library'
 
 app.use(bodyParser.json())
+
+app.get('/note', (req, res) => {
+  MongoClient.connect(url, (err, db) => {
+    if (err) {
+      console.error(err)
+      res.sendStatus(500)
+      process.exit(1)
+    }
+    db.collection('note')
+      .find({})
+      .toArray()
+      .then((contents) => res.json(contents))
+      .catch((err) => {
+        console.error(err)
+        res.sendStatus(500)
+      })
+      .then(() => db.close())
+  })
+})
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
